27930cc4d66f7a1f26217b9c9a076eedf5b808a2
Mitsuki0619 <boc.fsfb@gmail.com>
2025-02-13 22:44:03 +0900

    feat: update event time formatting to use timezone-aware functions

--- src/app/schedule/calendar/@modal/(...)schedule/[scheduleId]/edit/page.tsx
+++ src/app/schedule/calendar/@modal/(...)schedule/[scheduleId]/edit/page.tsx
@@ -2,7 +2,7 @@ import { editEvent } from "@/app/schedule/[scheduleId]/edit/actions";
 import { fetchSchedule } from "@/app/schedule/[scheduleId]/loaders";
 import { ScheduleEventForm } from "@/app/schedule/components/ScheduleEventForm/ScheduleEventForm.server";
 import ScheduleEventFormModal from "@/app/schedule/components/ScheduleEventFormModal/ScheduleEventFormModal";
-import { format } from "date-fns";
+import { formatInTimeZone } from "date-fns-tz";
 
 interface Params {
   scheduleId: string;
@@ -26,8 +26,16 @@ export default async function EditScheduleModal({
             title: schedule.title,
             categoryId: schedule.categoryId,
             description: schedule.description,
-            startTime: format(schedule.startTime, "yyyy-MM-dd HH:mm:ss"),
-            endTime: format(schedule.endTime, "yyyy-MM-dd HH:mm:ss"),
+            startTime: formatInTimeZone(
+              schedule.startTime,
+              "Asia/Tokyo",
+              "yyyy-MM-dd HH:mm:ss"
+            ),
+            endTime: formatInTimeZone(
+              schedule.endTime,
+              "Asia/Tokyo",
+              "yyyy-MM-dd HH:mm:ss"
+            ),
             note: schedule.note,
             tasks:
               schedule.tasks?.map((task) => ({
